4144160b69eefedaaba669e953092fba091d425b
Leo Lamprecht <mindrun@icloud.com>
2020-07-23 13:05:10 +0200

    Logged Environment Variables

--- api/test.js
+++ api/test.js
@@ -1,10 +1,11 @@
 module.exports = (req, res) => {
-    console.log(process.env.DEFINED_IN_CLOUD)
+    console.log('---- api/test.js ----')
 
-    console.log(process.env.NOW_URL)
-    console.log(process.env.NOW_REGION)
-    console.log(process.env.VERCEL_URL)
-    console.log(process.env.VERCEL_REGION)
+    console.log(`DEFINED_IN_CLOUD: ${process.env.DEFINED_IN_CLOUD}`)
+    console.log(`NOW_URL: ${process.env.NOW_URL}`)
+    console.log(`NOW_REGION: ${process.env.NOW_REGION}`)
+    console.log(`VERCEL_URL: ${process.env.VERCEL_URL}`)
+    console.log(`VERCEL_REGION: ${process.env.VERCEL_REGION}`)
 
     res.json({
         haha: true
--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 const http = require('http')
 
 http.createServer(function (req, res) {
-    console.log(process.env.DEFINED_IN_CLOUD)
+    console.log('---- server.js ----')
 
-    console.log(process.env.NOW_URL)
-    console.log(process.env.NOW_REGION)
-    console.log(process.env.VERCEL_URL)
-    console.log(process.env.VERCEL_REGION)
+    console.log(`DEFINED_IN_CLOUD: ${process.env.DEFINED_IN_CLOUD}`)
+    console.log(`NOW_URL: ${process.env.NOW_URL}`)
+    console.log(`NOW_REGION: ${process.env.NOW_REGION}`)
+    console.log(`VERCEL_URL: ${process.env.VERCEL_URL}`)
+    console.log(`VERCEL_REGION: ${process.env.VERCEL_REGION}`)
 
     res.write('Hello World!')
     res.end()
