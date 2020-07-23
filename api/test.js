96c9445cd1d06d0d07140271680a78e8494b5b4d
Leo Lamprecht <mindrun@icloud.com>
2020-07-23 13:02:02 +0200

    Fixed all Environment Variables

--- api/test.js
+++ api/test.js
@@ -1,6 +1,10 @@
 module.exports = (req, res) => {
     console.log(process.env.DEFINED_IN_CLOUD)
+
+    console.log(process.env.NOW_URL)
+    console.log(process.env.NOW_REGION)
     console.log(process.env.VERCEL_URL)
+    console.log(process.env.VERCEL_REGION)
 
     res.json({
         haha: true
--- server.js
+++ server.js
@@ -1,9 +1,13 @@
-const http = require('http');
+const http = require('http')
 
 http.createServer(function (req, res) {
     console.log(process.env.DEFINED_IN_CLOUD)
-    console.log(process.env.VERCEL_URL)
 
-    res.write('Hello World!');
-    res.end();
-}).listen(process.env.PORT);
+    console.log(process.env.NOW_URL)
+    console.log(process.env.NOW_REGION)
+    console.log(process.env.VERCEL_URL)
+    console.log(process.env.VERCEL_REGION)
+
+    res.write('Hello World!')
+    res.end()
+}).listen(process.env.PORT)
